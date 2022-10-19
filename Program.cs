f3b79cdc053d66d695953f05b9d60c06b572c345
irvina32 <115579967+irvina32@users.noreply.github.com>
2022-10-19 18:19:37 +0300

    задача №2 к Лекции №1.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("введите первое число: ");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.Write("введите второе число: ");
+int n2 = int.Parse(Console.ReadLine());
+if (n1 > n2)
+{
+    Console.WriteLine ("Первое число больше второго");
+}
+
+    else 
+    if (n1 < n2)
+    {
+       
+       Console.WriteLine ("Первое число меньше второго"); 
+    }
+    else
+    {
+        Console.WriteLine ("Оба числа равны");
+    }
