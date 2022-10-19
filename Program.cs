d2314b0373fd701bcfc81c53609ea13c2576672a
irvina32 <115579967+irvina32@users.noreply.github.com>
2022-10-19 18:24:31 +0300

    Задача №8 к лекции №1

--- Program.cs
+++ Program.cs
@@ -1,30 +1,11 @@
-﻿
-
-Console.Write("введите число: ");
+﻿Console.Write("введите число: ");
 
 int n = int.Parse(Console.ReadLine());
-//int a[n];
-//n = Console.Read;
 
-if (n % 2==0) 
-    {
+for (int i = 1; i <= n; i++){
+if (i % 2==0){ 
     
-    Console.WriteLine("да");
-
-    }
-
-    else
-
-    { 
-        Console.WriteLine("нет");
-    }
-//int n = ("четное");
-
-//else
-//{
-    // int n = ("нечетное");
-    //Console.WriteLine(n);
-//}
-
-
-
+    
+    Console.WriteLine(i);
+}
+}
