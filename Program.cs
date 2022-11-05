4a1319197f92931100fe8425bbb52486a7c16cc4
irvina32 <115579967+irvina32@users.noreply.github.com>
2022-11-05 14:43:20 +0300

    Семинар №5

--- Program.cs
+++ Program.cs
@@ -1,76 +1,122 @@
-﻿// Задача 28. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+﻿// // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// // Напишите программу, которая покажет количество чётных чисел в массиве.
+// //[345, 897, 568, 234] -> 2
 
-void PrintMassiv()
-{
-int[] N = new int[8];
-Console.Write("Массив [");
-for (int i = 0; i < N.Length; i++)
-{
-N[i] = new Random().Next(0,99);
-Console.Write($"{N[i]} ");
+
+
+int[] CreateArray(int size, int Min, int max)
+ { int[] array = new int[size];
+ Random rnd = new Random();
+for (int i = 0; i < size; i++)
+ { array[i] = rnd.Next(Min, max);
 }
-Console.Write("]");
+return array;
 }
-PrintMassiv();
 
-
-
-// Задача 25.  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-
-Console.Write("Введите число A: ");
-int numberA = int.Parse(Console.ReadLine());
-
-Console.Write("Введите число B: ");
-int numberB = int.Parse(Console.ReadLine());
-
-Console.WriteLine($"результат равен: {Math.Pow(numberA,numberB)}");
-
-
-
-Console.Write("Введите число A: ");
-int  numberA = int.Parse(Console.ReadLine());
-
-Console.Write("Введите число B: ");
- int numberB = int.Parse(Console.ReadLine());
- 
-
- int GetCount(int numberA, int numberB){
-
-  int result = 1;
-  for(int i=1; i <= numberB; i++){
-    result = result * numberA;
-  }
-    
-    return result;
-}
-   int  Numbers = GetCount(numberA, numberB);
-  Console.WriteLine($"результат равен: {GetCount(numberA, numberB)}");
-
-
-
-// Задача 27.  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
-int number = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Сумма цифр = " + GetLength(number));
-
-
-int GetLength(int number)
+int EvenNumber(int[] array)
 {
+    int count = 0;
+    for ( int i =0; i < array.Length; i++)
+     {
+        if (array[i] % 2 == 0) count++;
+     }
+     return count;
+}
 
+string GetArray(int[] array)
+{
+    string count = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        count = count + $"{array[i]}, ";
+    }
+    return count;
+}
+int[] array  = CreateArray(10, 100, 1000);
+
+System.Console.WriteLine(GetArray(array));
+System.Console.WriteLine(EvenNumber(array));
+
+
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateArray(int size, int Min, int max)
+ { int[] array = new int[size];
+ Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+ { array[i] = rnd.Next(Min, max);
+}
+return array;
+}
+
+int[] SumOddNumber(int[] array){
 int sum = 0;
-int result = 0;
-
-for(int i = 0; i <= number; i++)
+for (int i =0; i < array.Length; i++)
 {
-    sum = number -  number % 10;
-    result = result + (number - sum);
-    number = number / 10;
+    if (array[i] % 2 != 0) sum = sum + array[i];
+ }  
+  return array;
 }
 
-return result;
-
+string GetArray(int[] array)
+{
+    string sum = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        sum = sum + $"{array[i]}, ";
+    }
+    return sum;
 }
-int sumN = GetLength(number);
+int[] array  = CreateArray(4, 1, 10);
+
+
+
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу 
+// между максимальным и минимальным элементов массива.
+
+int[] CreateArray(int size, int Min, int max)
+ { int[] array = new int[size];
+ Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+ { array[i] = rnd.Next(Min, max);
+}
+return  array;
+}
+
+double DifElements(int[] array){
+
+double MinElement = array[0];
+double MaxElement = array[0];
+ for (int i = 0; i < array.Length; i++)
+ {
+    if (array[i] < MinElement) 
+    {                
+         MinElement = array[i];}
+     
+     if (array[i] > MaxElement)
+    {
+         MaxElement = array[i];}
+    }
+   double dif = MaxElement - MinElement;
+
+  return dif;  
+}
+string GetArray(int[] array)
+{
+    string dif = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        dif = dif + $"{array[i]}, ";
+    }
+    return dif;
+}
+
+int[] array  = CreateArray(5, 1, 30);
+Console.WriteLine(GetArray(array));
+Console.WriteLine("Разница между минимальным и максимальным элементом массива = " + DifElements(array));
 
 
