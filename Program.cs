968c2bff584145de9c507b74d746e53ed91dd6eb
irvina32 <115579967+irvina32@users.noreply.github.com>
2023-01-14 13:10:14 +0300

    СЕМИНАР №9. ИТОГОВЫЙ ПРОЕКТ

--- Program.cs
+++ Program.cs
@@ -1,88 +1,106 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
-// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// See https://aka.ms/new-console-template for more information
+// 
+
+/*Задача 64: Задайте значение N. Напишите программу, которая выведет все
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+// Console.WriteLine("Введите натуральное число больше 1:");
+// int counter = Math.Abs(Convert.ToInt32(Console.ReadLine())); 
+// if (counter == 0) { Console.WriteLine("Вы ввели не натуральное число!");} 
+// else
+// {
+//     void NumberCounter(int counter)
+//     {
+//         if (counter == 0) return;
+//         Console.Write(counter + " ");
+//         NumberCounter(counter - 1);
+//     }
+
+//     NumberCounter(counter);
+// }
 
 
 
-Console.Write("Введите числа через запятую: ");
-int[] numbers = StringToNum(Console.ReadLine());
-PrintArray(numbers);
-int sum = 0;
-for (int i = 0; i < numbers.Length; i++)
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+// Console.Write("Введите начальное целое число M нижнего дипазона значений: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+
+// if (M < 1) M = 1;
+
+// Console.Write("Введите конечное целое число N верхнего дипазона значений: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// if (N < 1)
+// {
+//     Console.Write("В заданном Вами диапазоне отсутствуют натуральные числа");
+// }
+// else
+// {
+
+//     SumFromMToN(M, N);
+
+  
+//     void SumFromMToN(int m, int n)
+//     {
+//         Console.Write("Сумма натуральных чисел в заданном диапазоне: " + SumMN(M - 1, N));
+//     }
+
+   
+//     int SumMN(int M, int N)
+//     {
+//         int result = M;
+//         if (M == N)
+//             return 0;
+//         else
+//         {
+//             M++;
+//             result = M + SumMN(M, N);
+//             return result;
+//         }
+//     }
+// }
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите число M: ");
+int m = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+Console.Write("Введите число N: ");
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+AkkermanFunction(m,n);
+
+
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
 {
-    if (numbers[i] > 0)
+    Console.Write(Akk(m, n)); 
+}
+
+// функция Аккермана
+int Akk(int m, int n)
+{
+    if (m == 0)
     {
-        sum++;
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akk(m - 1, 1);
+    }
+    else
+    {
+        return (Akk(m - 1, Akk(m, n - 1)));
     }
 }
-Console.WriteLine();
-Console.WriteLine($"количество значений больше 0 = {sum}");
-
-
-int[] StringToNum(string input)
-{
-    int count = 1;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ',')
-        {
-            count++;
-        }
-    }
-
-    int[] numbers = new int [count];
-    int index = 0;
-
-    for (int i = 0; i < input.Length; i++)
-    {
-        string temp = "";
-
-        while (input [i] != ',')
-        {
-        if(i != input.Length - 1)
-        {
-            temp += input [i].ToString();
-            i++;
-        }
-        else
-        {
-            temp += input [i].ToString();
-            break;
-        }
-        }
-        numbers[index] = Convert.ToInt32(temp);
-        index++;
-    }
-    return numbers;
-}
-
-
-void PrintArray(int[] array)
-{
-    Console.Write("[ ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.Write("]");
-}
-
-
-
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух 
-// прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения
-//  b1, k1, b2 и k2 задаются пользователем.
-
-Console.WriteLine("введите значение b1");
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k1");
-double k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите значение b2");
-double b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число k2");
-double k2 = Convert.ToInt32(Console.ReadLine());
-
-double x = (-b2 + b1) / (-k1 + k2);
-double y = k2 * x + b2;
-
-Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
-
